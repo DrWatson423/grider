6fd2bea19e6518df9d36cb056732bd5b19d0c3b1
DrWatson423 <elvisadjeiboateng20@gmail.com>
2026-08-13 03:32:49 +0000

    fix: configure cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,50 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes.js"
-import riderRoutes from "./routes/riderRoutes.js"
 
-
+import authRoutes from "./routes/authRoutes.js";
+import riderRoutes from "./routes/riderRoutes.js";
 
 const app = express();
 
-app.use(cors());
-app.use((req, res, next) => {
-  console.log("METHOD:", req.method);
-  console.log("URL:", req.url);
-  console.log("CONTENT TYPE:", req.headers["content-type"]);
-  next();
-});
 
+// CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://grider.adjeiboatengelvis423.workers.dev/"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
+
+// Body parser
 app.use(express.json());
 
 
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "Grider API Running"
-    });
+// Request logger
+app.use((req, res, next) => {
+  console.log("METHOD:", req.method);
+  console.log("URL:", req.url);
+  console.log("ORIGIN:", req.headers.origin);
+  next();
 });
 
+
+// Root
+app.get("/", (req, res) => {
+  res.json({
+    message: "Grider API Running"
+  });
+});
+
+
+// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/riders",riderRoutes);
+app.use("/api/riders", riderRoutes);
+
 
 export default app;
